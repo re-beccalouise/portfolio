80eab402881de22a18d7f650faab99f4d9850195
re-beccalouise <rebeccamoorcroft@outlook.com>
2022-12-30 11:33:26 +0000

    added toggle to home page

---
+++ index.js
@@ -0,0 +1,14 @@
+// let toggleBtn = document.getElementById('toggle-dark');
+
+
+
+function toggleMode () {
+    let button = document.body;
+    button.classList.toggle('dark-mode');
+}
+
+
+
+// toggleBtn.src = './public/moon-light.png';
+// body.style.backgroundColor = '#4B5842';
+// body.style.color = '#C7F9CC';
